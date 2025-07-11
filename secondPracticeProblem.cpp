f1ac5bd7a2458b50d331c922148e5633ce12af6e
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-07-11 06:37:43 +0600

    Solved second practice problem

---
+++ secondPracticeProblem.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int integerValue;
+    Node *previousPointer;
+    Node *nextPointer;
+
+    Node(int integerValue)
+    {
+        this->integerValue = integerValue;
+        this->previousPointer = NULL;
+        this->nextPointer = NULL;
+    }
+};
+
+void insertAtTail(Node *&head, Node *&tail, int integerValue)
+{
+    Node *newNode = new Node(integerValue);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+    }
+    else
+    {
+        tail->nextPointer = newNode;
+        newNode->previousPointer = tail;
+        tail = newNode;
+    }
+}
+
+void reverseLinkedList(Node *head, Node *tail)
+{
+    for (Node *i = head, *j = tail; j != i->previousPointer && i != j; i = i->nextPointer, j = j->previousPointer)
+    {
+        swap(i->integerValue, j->integerValue);
+    }
+}
+
+void printForward(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->integerValue << " ";
+        tmp = tmp->nextPointer;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int inputValue;
+    while (true)
+    {
+        cin >> inputValue;
+        if (inputValue == -1)
+        {
+            break;
+        }
+        else
+        {
+            insertAtTail(head, tail, inputValue);
+        }
+    }
+
+    reverseLinkedList(head, tail);
+    printForward(head);
+
+    return 0;
+}
